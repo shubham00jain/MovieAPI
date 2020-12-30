b1708bac71da79d6d3a610559dd1542ff46f735f
Shubham656jain <shubham656jain@gmail.com>
2020-12-30 21:01:55 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const { static } = require('express');
+const express = require('express');
+const app = express();
+const request = require('request');
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.redirect("/search");
+});
+
+
+app.get("/search", function(req, res){
+    res.render("search");
+});
+
+app.get("/movies", function(req, res){
+    const query = req.query.i;
+    console.log(req);
+    let url = "http://www.omdbapi.com/?apikey=b63af212&i=" + query;
+    request(url, function(err,response, body){
+        const data = JSON.parse(body);
+        if(!err && response.statusCode == 200){
+            res.render("movie", {data:data});
+        }
+    });
+});
+
+
+app.get("/results", function(req, res){
+
+    const query = req.query.movie;
+    const url = "http://www.omdbapi.com/?apikey=b63af212&s=" + query; 
+
+    request(url, function(err, response, body){
+        if(!err && response.statusCode == 200){
+            const vari = JSON.parse(body);
+            res.render("results", {data:vari});
+        }
+    });
+});
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000.")
+})
