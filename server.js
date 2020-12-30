770e1c8467d16b5f335ab8d090a81c4516ed05ba
Shubham656jain <shubham656jain@gmail.com>
2020-12-30 21:16:02 +0530

    make it better

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.get("/results", function(req, res){
 });
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server started on port 3000.")
 })
